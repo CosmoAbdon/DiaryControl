4dc6d6db225ccde3db8046c5c365e02be9d2974c
Igor <Igor@192.168.2.101>
2015-08-16 01:08:43 -0300

    16/08/2015 - Criação da entidade Usuário 16/08/2015 - Criação do form LoginForm

---
+++ src/Entity/Usuario.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity;
+
+/**
+ *
+ * @author Igor
+ */
+public class Usuario {
+    
+    private Integer id;
+    private String login;
+    private String senha;
+    private String nome;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+    
+}
